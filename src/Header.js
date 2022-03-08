10cc8c1654e84d55690b8477b1d0ff9c13448f62
Daniel Williams <danielpaulwill@gmail.com>
2022-03-07 21:48:41 -0500

    further dark mode refinements

--- src/Header.js
+++ src/Header.js
@@ -24,7 +24,7 @@ function Header({ isDarkMode, handleDarkMode }) {
       <img id="logo" alt="Crypto Finder App" src="https://lh3.googleusercontent.com/pw/AM-JKLX1SnTe11oqSOkI-fXtnB0UwM6LGeBJGcA8H9xHSWKdlaGoa6afIJ_CgRFjFkSkltDaC50mQJV1eD3wQ7nwEQaXoGFPQapvoj1yR0X7VE5miO3j7fwKdJRYkaRCqGY2-ri_6Qlpd0ZEDcOwOgfckFn_bg=w300-h200-no?authuser=0"></img>
       <h1 id="title">Crypto Finder</h1>
     </div>
-    <div className="container">
+    <div className={isDarkMode ? "darkModeContainerL" : "darkModeContainerD"}>
 
         <p>Dark Mode</p>
       
