2204fe9dc984c32e32a8bc5771ae5c9e1c80824a
Daniel Williams <danielpaulwill@gmail.com>
2022-11-29 17:19:20 -0500

    fixed logo bug

--- src/Header.js
+++ src/Header.js
@@ -21,7 +21,7 @@ function Header({ isDarkMode, handleDarkMode }) {
   return (
   <div>
     <div id={isDarkMode ? "headerL" : "headerD"}>
-      <img id="logo" alt="Crypto Finder App" src="https://lh3.googleusercontent.com/pw/AM-JKLX1SnTe11oqSOkI-fXtnB0UwM6LGeBJGcA8H9xHSWKdlaGoa6afIJ_CgRFjFkSkltDaC50mQJV1eD3wQ7nwEQaXoGFPQapvoj1yR0X7VE5miO3j7fwKdJRYkaRCqGY2-ri_6Qlpd0ZEDcOwOgfckFn_bg=w300-h200-no?authuser=0"></img>
+      <img id="logo" alt="Crypto Finder App" src="https://raw.githubusercontent.com/danielpaulwill/crypto-info-app/main/Assets/AppFinder.gif"></img>
       <h1 id="title">Crypto Finder</h1>
     </div>
     <div className={isDarkMode ? "darkModeContainerL" : "darkModeContainerD"}>
