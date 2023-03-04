e45fd609c5d7a8e65df9fec57a20f0a18e6711ac
Daniel Williams <danielpaulwill@gmail.com>
2023-03-03 22:14:22 -0500

    grid sizing of cards complete

--- src/CryptoCard.js
+++ src/CryptoCard.js
@@ -1,111 +1,74 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Paper from '@mui/material/Paper';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import { CardMedia } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function CryptoCard({ cryptoCoin, isDarkMode }) {
   const [expanded, setExpanded] = React.useState(false)
 
   const changeStatus = Math.sign(cryptoCoin.price_change_percentage_24h_in_currency)
 
-  interface ExpandMoreProps extends IconButtonProps {
-    expand: boolean;
-  }
-  
-  const ExpandMore = styled((props: ExpandMoreProps) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-  })(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+  const darkTheme = createTheme({ palette: { mode: 'dark' } });
+  const lightTheme = createTheme({ palette: { mode: 'light' } });
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    margin: theme.spacing(1)
+    // textAlign: 'center',
+    // color: theme.palette.text.secondary,
   }));
   
-  
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={<img className="image" src={cryptoCoin.image} alt="cryptocurrency thumbnail" ></img>}
-        title={cryptoCoin.name}
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} >
+        <Grid item xs={0} sm={1} md={2} >
+        </Grid>
+          <Grid item xs={12} sm={10} md={8} theme={darkTheme}>
+            <Item>
+              <img src={cryptoCoin.image} />
+              
+            </Item>
+          </Grid>
+        <Grid item xs={0} sm={1} md={2} >
+        </Grid>
+      </Grid>
+    </Box>
+
+    // <Card variant="outlined" theme={darkTheme}>
+
+    //   <CardContent>
+    //     <Avatar
+    //       alt="cryptocurrency thumbnail"
+    //       src={cryptoCoin.image}
+    //       sx={{ width: 56, height: 56 }}
+    //     />
+    //     <Typography variant="h6" component="div">
+    //       {cryptoCoin.name}
+    //     </Typography>
+    //     <Typography variant='subtitle1'>
+    //       {cryptoCoin.symbol}
+    //     </Typography>
+    //   </CardContent>
+      
+      
+    // </Card>
+
+
+
 
     // <div className="row">
     //   <div className="col-md-6">
