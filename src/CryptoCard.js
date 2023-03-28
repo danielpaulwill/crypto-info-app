c6613c7b658981e92a46cf571496c0438ba9489a
Daniel Williams <danielpaulwill@gmail.com>
2023-03-27 22:48:46 -0400

    cards parsing correctly

--- src/CryptoCard.js
+++ src/CryptoCard.js
@@ -58,36 +58,6 @@ function CryptoCard({ cryptoCoin, isDarkMode }) {
         </Grid>
       </Grid>
     </Box>
-
-
-// <div className="row">
-//   <div className="col-md-6">
-// <div className={isDarkMode ? "row cardD" : "row cardL"}>
-
-    //       <div className="col-2">
-    //         <img className="image" src={cryptoCoin.image} alt="cryptocurrency thumbnail" ></img>
-    //       </div>
-    //       <div className="col-2">
-    //         <p className="name"><b>{cryptoCoin.name}</b></p>
-    //       </div>
-    //       <div className="col-2">
-    //         <p className="ticker">{cryptoCoin.symbol}</p>
-    //       </div>
-    //       <div className="col-3">
-    //         <p className={changeStatus === 1 ? "arrowUp" : "arrowDown"}>{changeStatus === 1 ? "▲" : "▼"}</p>
-    //         <p className={changeStatus === 1 ? "positive" : "negative"}>
-    //           {changeStatus === 1 ? "+" : "-"}
-    //           ${changeStatus === 1 ? cryptoCoin.price_change_24h.toFixed(2) : Math.abs(cryptoCoin.price_change_24h.toFixed(2))}
-    //           <br></br>({cryptoCoin.price_change_percentage_24h.toFixed(2)}%)
-    //         </p>  
-    //       </div>
-    //       <div className="col-3">
-    //         <p className="child">${cryptoCoin.current_price.toFixed(2)}</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="col-md-3"></div>
-    // </div>
   )
 };
 
--- src/CryptoList.js
+++ src/CryptoList.js
@@ -1,68 +1,12 @@
 import { useEffect, useState } from "react";
 import CryptoCard from "./CryptoCard"
-import { DataGrid } from '@mui/x-data-grid';
-import Box from '@mui/material/Box';
 
 function CryptoList({ isDarkMode }) {
   const [cryptoData, setCryptoData] = useState([])
   const [cryptoList, setCryptoList] = useState()
 
   console.log(cryptoData)
-
-  const columns = [
-    {
-      field: 'image',
-      headerName: '',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'name',
-      headerName: 'Name',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'symbol',
-      headerName: '',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'current_price',
-      headerName: 'Price',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'change',
-      headerName: 'Change',
-      type: 'number',
-      width: 110,
-      editable: true,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+  console.log(cryptoList)
 
   function initFetch() {
     fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=24h")
@@ -73,39 +17,27 @@ function CryptoList({ isDarkMode }) {
   useEffect(() => {
     initFetch()
     setInterval(initFetch, [60000])
-  }, [])  
+  }, [])
 
-  const cryptoCards = cryptoData.map((cryptoCoin, index) => (
-          <CryptoCard 
-            key={cryptoCoin.id} 
-            cryptoCoin={cryptoCoin} 
-            isDarkMode={isDarkMode}
-          />
-          ))
+  useEffect(() => {
+    if (cryptoData !== []) {
+      let cryptoCards = cryptoData.map(cryptoCoin => <CryptoCard key={cryptoCoin.id} cryptoCoin={cryptoCoin} isDarkMode={isDarkMode} />)
+      setCryptoList(cryptoCards)
+    }
+  }, [cryptoData])
+
+  // const cryptoCards = cryptoData.map((cryptoCoin, index) => (
+  //         <CryptoCard 
+  //           key={cryptoCoin.id} 
+  //           cryptoCoin={cryptoCoin} 
+  //           isDarkMode={isDarkMode}
+  //         />
+  //         ))
 
   return (
-    // <div className="container-fluid">
-    //     {cryptoCards}
-    // </div>
-
-
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={cryptoData}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
-          },
-        }}
-        // pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
-
+    <div className="container-fluid">
+        {cryptoList}
+    </div>
 
   )
 };
