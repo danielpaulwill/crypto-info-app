7420c85e471e930f9ae2a43825f9f4c98680c839
Daniel Williams <danielpaulwill@gmail.com>
2023-05-08 10:31:35 -0400

    created hexagon shape for images

--- src/CryptoCard.js
+++ src/CryptoCard.js
@@ -1,10 +1,16 @@
 function CryptoCard({ cryptoCoin, isDarkMode }) {
 
+  console.log(cryptoCoin)
+
   const changeStatus = Math.sign(cryptoCoin.price_change_percentage_24h_in_currency)
 
 
   return (
     <div className="cardD">
+      <div className="imgContainer">
+
+      <img src={cryptoCoin.image} className="cardImg"></img>
+      </div>
       Hello World!
     </div>
   )
